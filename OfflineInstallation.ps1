b05ce7f12bce0708837762f853bc5628ed1b4102
rakelord <magnus.schollin@rts.se>
2024-01-26 14:21:33 +0100

    Added OfflineInstallation

---
+++ OfflineInstallation.ps1
@@ -0,0 +1,8 @@
+$PSModulePath = "$HOME\Documents\WindowsPowerShell\Modules"
+$Files = Get-ChildItem -Path ".\" -Include ("*.psm1","*.psd1") -recurse
+$ModuleName = "PSTopdeskFunctions"
+$NewModulePath = $PSModulePath+"\"+$ModuleName
+if (!(Test-Path -Path "$PSModulePath")){
+    New-Item -Path $NewModulePath -ItemType Directory -Force
+}
+$Files | Copy-Item -Destination $NewModulePath -Force -Confirm:$false
